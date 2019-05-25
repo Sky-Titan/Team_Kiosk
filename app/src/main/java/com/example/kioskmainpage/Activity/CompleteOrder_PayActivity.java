0d4c1fd47d2de24cebc8b7261081d2f358178e77
Sky-Titan <er65119@naver.com>
2019-05-25 13:53:47 +0900

    UI modify complete

--- app/src/main/java/com/example/kioskmainpage/Activity/CompleteOrder_PayActivity.java
+++ app/src/main/java/com/example/kioskmainpage/Activity/CompleteOrder_PayActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.GridLayout;
 import android.widget.TextView;
 
 
